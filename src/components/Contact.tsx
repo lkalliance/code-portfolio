bbae1b033169f3c85cc245806ec4505c6aa9f042
lkalliance <119076402+lkalliance@users.noreply.github.com>
2023-04-29 15:12:18 -0500

    Created basic form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -3,6 +3,18 @@ function Contact() {
   return (
     <section>
       <h3>Contact Me</h3>
+      <form>
+        <label htmlFor="Name">Name</label>
+        <input type="text" id="Name" />
+        <label htmlFor="email">Email</label>
+        <input type="text" id="email" />
+        <label htmlFor="message">Message</label>
+        <textarea id="message"></textarea>
+        <button type="submit" onClick={(e: React.MouseEvent) => {
+          e.preventDefault();
+          alert('Sending a message');
+        }}>Send</button>
+      </form>
     </section>
   )
 }
